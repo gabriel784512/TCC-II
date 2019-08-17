074218a06586c9a2b650e86296c1ea7b73b16bad
Gabriel Zani <gabrielsouza8a2.opo@gmail.com>
2019-08-16 21:45:25 -0400

    Alteracoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,9 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -20,7 +22,10 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    SpeechRecognition,
+    TextToSpeech,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ScreenOrientation
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { TextToSpeech } from '@ionic-native/text-to-speech/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  matches: Array<String>;
 
-  constructor() {}
+  constructor(private speechRecognition: SpeechRecognition, private screenOrientation: ScreenOrientation, private statusBar: StatusBar, private tts: TextToSpeech) {
+  }
+
+  ngOnInit(){
+
+    // Definindo modo retrato
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
+
+    // Deixa o texto claro no statusBar
+    this.statusBar.styleBlackOpaque();
+
+    // Verificando a permissao de voz
+    this.speechRecognition.hasPermission()
+    .then((hasPermission: boolean) => {
+      if (!hasPermission){
+        // Solicita permissao
+        this.speechRecognition.requestPermission()
+        .then(
+          () => console.log('Granted'),
+          () => console.log('Denied')
+        )
+      }
+    });
+  }
+
+  startVoz(){
+    let options = {
+      language: 'pt-BR',
+      matches: 1,
+      prompt: 'Estou te ouvindo! :)',  
+      showPopup: true,                
+    }
+    // Processo de reconhecimento de voz
+    this.speechRecognition.startListening(options).subscribe(matches => {
+      this.matches = matches,      
+      (onerror) => console.log('error:', onerror)
+    })
+  }
+
+  playVoz(){
+    // Processo de tocar voz
+    this.tts.speak({
+      text : this.matches.toString(),
+      locale: 'pt-BR',
+      rate: 1
+    }).then(() => console.log('Success'))
+    .catch((reason: any) => console.log(reason));
+  }
 
 }
+
